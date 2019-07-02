4772af26a5a661181d6747c1e91e15e08dd95027
nguyen dang <dangnguyenminh171@gmail.com>
2019-07-02 15:11:10 +0700

    de3

--- gitcomplete/ViewController.swift
+++ gitcomplete/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("develop")
         print("develop1")
+        print("dangminhgnuyen")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
